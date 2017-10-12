e76ee2b0160a2100ab2627520717f2d5a5f1b5b1
erkajp <erka.j.p@gmail.com>
2017-10-12 22:12:18 +0700

    add drawer

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 // import { StyleSheet, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, DrawerNavigator } from 'react-navigation';
 import Home from './src/Home';
 import Keranjang from './src/Keranjang';
 import Suka from './src/Suka';
+import Pengaturan from './src/Pengaturan';
 
-const Naviagtion = TabNavigator({
+const Tab = TabNavigator({
   Home: { screen: Home },
   Keranjang: { screen: Keranjang },
   Suka: { screen: Suka }
 });
 
+const Drawer = DrawerNavigator({
+  tab: { screen: Tab },
+  pengaturan: { screen: Pengaturan }
+});
+
 export default class App extends React.Component {
   render() {
-    return (
-      <Naviagtion />
-    );
+    return <Drawer />;
   }
 }
---
+++ src/Pengaturan.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Pengaturan extends Component {
+  static navigationOptions = {
+    drawerLabel: 'Pengaturan'
+  };
+  render() {
+    return <Text>This is Pengaturan</Text>;
+  }
+}
+
+export default Pengaturan;
