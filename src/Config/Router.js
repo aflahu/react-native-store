4aa87bb21075cecd0940f1218c45bfff549eabd4
erkajp <erka.j.p@gmail.com>
2017-10-14 16:29:09 +0700

    styling header

--- src/Config/Router.js
+++ src/Config/Router.js
@@ -4,23 +4,45 @@ import Keranjang from './../Screens/Keranjang';
 import Suka from './../Screens/Suka';
 import Pengaturan from './../Screens/Pengaturan';
 
-const Tab = TabNavigator({
-  Home: {
-    screen: Home
+const Tab = TabNavigator(
+  {
+    Home: {
+      screen: Home
+    },
+    Keranjang: {
+      screen: Keranjang
+    },
+    Suka: {
+      screen: Suka
+    }
   },
-  Keranjang: {
-    screen: Keranjang
-  },
-  Suka: {
-    screen: Suka
+  {
+    tabBarOptions: {
+      activeTintColor: 'black',
+      inactiveTintColor: 'grey',
+      style: {
+        backgroundColor: 'white'
+      },
+      labelStyle: {
+        fontSize: 12
+      },
+      indicatorStyle: {
+        backgroundColor: 'black'
+      }
+    }
   }
-});
+);
 
 const Sn = StackNavigator({
   tab: {
     screen: Tab,
     navigationOptions: {
-      title: 'Store'
+      title: 'Store',
+      headerStyle: {
+        backgroundColor: 'white',
+        paddingTop: 17,
+        elevation: 0
+      },
     }
   },
   pengaturan: {
